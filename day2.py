9e792e36ce03891917bc5dd9d4dd91d9f1e67144
aaryan174 <aryan007.lko@gmail.com>
2026-08-16 02:08:16 +0530

    Refactor to calculate and display the average of two user-input numbers

--- day2.py
+++ day2.py
@@ -1,7 +1,10 @@
 #sum of two number by user input
+#
+
+# avg of number
 
 a = int(input("Enter the number a:"))
 b = int(input("Enter the number b:")) 
 
-sum = a + b
-print("the sum of a and b is", sum)
+avg = (a + b)/2
+print("the avg of two number is:", avg, type(avg))
