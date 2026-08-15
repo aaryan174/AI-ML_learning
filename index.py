0f736c358f78ddf02a85b0c8b4d54e402a1f1669
aaryan174 <aryan007.lko@gmail.com>
2026-08-15 23:24:25 +0530

    learning python for ai /ml

---
+++ index.py
@@ -0,0 +1,19 @@
+# "and" operator
+
+# citizenShip = input("Enter your citizenship")
+# age = int(input("enter your age"))
+# if citizenShip == "India" and age >= 18:
+#     print("you  can vote")
+# else: 
+#     print("you can not vote")
+
+# "OR" Operator
+ 
+name = input("ENter your  name")
+marks = int(input("enter your marks"))
+CU_result = input("enter your result").lower()
+
+if marks >= 75 or CU_result == 'Pass':
+    print(f"hi! {name} Welcome in chandigarh university. you are passsed")
+else:
+    print(f"hi!! {name} fail ho gye bhai tum")
