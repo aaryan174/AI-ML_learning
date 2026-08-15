d90263ae9ac5cf73be27568bdacf7e675fbd8b62
aaryan174 <aryan007.lko@gmail.com>
2026-08-16 02:28:38 +0530

    Remove average calculation and update multiple of 5 check

--- day2.py
+++ day2.py
@@ -3,8 +3,16 @@
 
 # avg of number
 
-a = int(input("Enter the number a:"))
-b = int(input("Enter the number b:")) 
+# a = int(input("Enter the number a:"))
+# b = int(input("Enter the number b:")) 
 
-avg = (a + b)/2
-print("the avg of two number is:", avg, type(avg))
+# avg = (a + b)/210
+# print("the avg of two number is:", avg, type(avg))
+
+
+#wap multipal of 5
+a = int(input("Enter the number a:"))
+if (a%5==0):
+    print(a, "is a multiple of 5")
+else:
+    print(a, "is not multiple of 5")
