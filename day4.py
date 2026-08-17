9bbc2db295851ca41c51451224f3496dc90e5050
aaryan174 <aryan007.lko@gmail.com>
2026-08-17 23:33:46 +0530

    understanding of list method like apend, insert, sort, reverse

--- day4.py
+++ day4.py
@@ -1,4 +1,14 @@
 
 marks = [56, 54, 56, 78,34,56,56,225,4564,564,64,45]
+
+marks.append(10)
 print(marks)
-print(len(marks))
+
+marks.insert(1, 2345)
+print(marks)
+
+marks.sort(reverse=True)
+print(marks)
+
+marks.reverse()
+print(marks)
