024bf03f3a225fdbcf4ebfe2056a9ef1663a99fe
aaryan174 <aryan007.lko@gmail.com>
2026-09-21 23:48:50 +0530

    2nd ques done

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
 let num = Number(prompt("enter the number"));
 
-if(num%2 == 0){
-    console.log(`the number is ${num} even`);
+if(num > 0){
+    console.log(`the number is ${num} positive`);
+}else if(num === 0){
+    console.log(`the number is ${num} `);
 }else{
-    console.log(`the number is ${num} odd`);
+    console.log("the num is negetive")
 }
